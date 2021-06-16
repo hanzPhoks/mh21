bba46abc04e00813d200a4c6080566c7a8fc0613
Jeanno <hanzPhoks@users.noreply.github.com>
2021-06-16 12:12:49 +0200

    init

---
+++ ressources/tilesets/logos.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="logos" tilewidth="32" tileheight="32" tilecount="2000" columns="40">
+ <image source="../../logos.png" width="1280" height="1600"/>
+ <wangsets>
+  <wangset name="barrière" type="edge" tile="72">
+   <wangcolor name="" color="#ff0000" tile="72" probability="1"/>
+   <wangtile tileid="28" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="29" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="30" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="31" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="32" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="33" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="68" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="69" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="70" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="71" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="72" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="73" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="110" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="111" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="112" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="113" wangid="1,0,1,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
