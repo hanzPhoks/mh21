3f0cf050cf8e0fe010e0681f27aab7fa6ac6d4f5
Jeanno <hanzPhoks@users.noreply.github.com>
2021-06-16 13:33:06 +0200

    rm tsx

--- ressources/tilesets/modern_interior.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="modern_interior" tilewidth="32" tileheight="32" tilecount="1424" columns="16">
- <image source="../../modern_interiors.png" width="512" height="2848"/>
-</tileset>
--- ressources/tilesets/retro_interior.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="retro_interior" tilewidth="32" tileheight="32" tilecount="768" columns="48">
- <image source="../../retro_interior.png" width="1536" height="512"/>
-</tileset>
