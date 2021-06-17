6f8743d776cbfe8a74e6f77bf08cb82467a214b8
Jeanno <hanzPhoks@users.noreply.github.com>
2021-06-17 17:15:04 +0200

    Tuto + fléchage

--- tuto.js
+++ tuto.js
@@ -2,12 +2,23 @@ let helloWorldPopup;
 
 // Open the popup when we enter a given zone
 helloWorldPopup = WA.onEnterZone('tuto-1', () => {
-    WA.openPopup("msg1", 'Coucou !', [{
+    WA.disablePlayerControls();
+    WA.openPopup("msg1", "Vous pouvez aussi dialoguer directement avec les autres jusqu'à 4.", [{
         label: "Fermer",
         className: "primary",
         callback: (popup) => {
             // Close the popup when the "Close" button is pressed.
             popup.close();
+            WA.restorePlayerControls();
+
+            WA.openPopup("msg1", "Pour discuter à plus que 4, vous trouvrerez des tables et des tentes réparties sur la map.", [{
+                label: "Fermer",
+                className: "primary",
+                callback: (popup) => {
+                    // Close the popup when the "Close" button is pressed.
+                    popup.close();
+                }
+            }]);
         }
     }]);
 });
