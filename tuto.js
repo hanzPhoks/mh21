67a15587322ef0da330a0ae017f67bfbb93afacb
Jeanno <hanzPhoks@users.noreply.github.com>
2021-07-06 17:53:47 +0200

    test dons et panneaux

--- tuto.js
+++ tuto.js
@@ -2,14 +2,14 @@ let helloWorldPopup;
 
 // Open the popup when we enter a given zone
 helloWorldPopup = WA.onEnterZone('tuto-1', () => {
-    WA.disablePlayerControls();
+    //WA.disablePlayerControls();
     WA.openPopup("msg1", "Vous pouvez aussi dialoguer directement avec les autres jusqu'à 4.", [{
         label: "Fermer",
         className: "primary",
         callback: (popup) => {
             // Close the popup when the "Close" button is pressed.
             popup.close();
-            WA.restorePlayerControls();
+            //WA.restorePlayerControls();
 
             WA.openPopup("msg1", "Pour discuter à plus que 4, vous trouvrerez des tables et des tentes réparties sur la map.", [{
                 label: "Fermer",
@@ -25,5 +25,24 @@ helloWorldPopup = WA.onEnterZone('tuto-1', () => {
 
 // Close the popup when we leave the zone.
 WA.onLeaveZone('tuto-1', () => {
+    helloWorldPopup.close();
+});
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('zone-dons', () => {
+    WA.openPopup("msg2", "Dieu nous a en effet mis à cœur de laisser l’entrée libre, afin que personne ne soit freiné pour des raisons financières. C’est donc grâce aux dons que chaque année le festival peut perdurer. Cette année, devenez vous aussi un partenaire du Festival en faisant un don à Event4Him.", [{
+        label: "Faire un don",
+        className: "primary",
+        callback: (popup) => {
+            WA.openTab("https://mitthim.fr/dons/");
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('zone-dons', () => {
     helloWorldPopup.close();
 });
