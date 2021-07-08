0a403279fb6ba4f727e9d8302769427ff4611835
hanzPhoks <hanzPhoks@users.noreply.github.com>
2021-07-08 09:50:52 +0000

    Deploying to gh-pages from master @ 266fa701ef4c74b13b821293b90d964f98f4005e 🚀

--- dons.js
+++ dons.js
@@ -1,7 +1,7 @@
-window.donsPopUp = null;
+let donsPopUp;
 
-window.donsPopUp = WA.onEnterZone('zone-1', () => {
-    WA.openPopup("msg1", "Dieu nous a en effet mis à cœur de laisser l’entrée libre, et c'est grâce aux dons que chaque année le festival peut se dérouler.", [{
+WA.onEnterZone('zone-1', () => {
+    donsPopUp = WA.openPopup("msg1", "Dieu nous a en effet mis à cœur de laisser l’entrée libre, et c'est grâce aux dons que chaque année le festival peut se dérouler.", [{
         label: "Faire un don",
         className: "primary",
         callback: (popup) => {
@@ -11,13 +11,13 @@ window.donsPopUp = WA.onEnterZone('zone-1', () => {
     }]);
 });
 WA.onLeaveZone('zone-1', () => {
-    window.donsPopUp.close();
+    donsPopUp.close();
 });
 
 
 
-window.donsPopUp = WA.onEnterZone('zone-2', () => {
-    WA.openPopup("msg2", "Dieu nous a en effet mis à cœur de laisser l’entrée libre, et c'est grâce aux dons que chaque année le festival peut se dérouler.", [{
+WA.onEnterZone('zone-2', () => {
+    donsPopUp = WA.openPopup("msg2", "Dieu nous a en effet mis à cœur de laisser l’entrée libre, et c'est grâce aux dons que chaque année le festival peut se dérouler.", [{
         label: "Faire un don",
         className: "primary",
         callback: (popup) => {
@@ -27,5 +27,5 @@ window.donsPopUp = WA.onEnterZone('zone-2', () => {
     }]);
 });
 WA.onLeaveZone('zone-2', () => {
-    window.donsPopUp.close();
+    donsPopUp.close();
 });
