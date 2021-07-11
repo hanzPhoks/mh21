315080f09ee9a6f321836baf256ee93e9cb9cde5
hanzPhoks <hanzPhoks@users.noreply.github.com>
2021-07-11 12:19:56 +0000

    Deploying to gh-pages from master @ e5574e9ddb707f9fa21c85c7daf00d1853def58f 🚀

--- mittshop.js
+++ mittshop.js
@@ -4,39 +4,48 @@ let arrayYT = [
     {
         "zone" : "yt-mirella",
         "lien" : "https://www.youtube.com/user/MirellaPageOfficiel",
-        "msgbox" : "yt1"
+        "msgbox" : "yt1",
+        "text" : "de Mirella"
     },{
         "zone" : "yt-sebastienGeoffrey",
         "lien" : "https://www.youtube.com/user/LAOHEAVENMUSIC",
-        "msgbox" : "yt2"
+        "msgbox" : "yt2",
+        "text" : "de Sebastien Geffroy"
     },{
         "zone" : "yt-alexiaRabe",
         "lien" : "https://www.youtube.com/user/alexiarabechannel",
-        "msgbox" : "yt3"
+        "msgbox" : "yt3",
+        "text" : "d'Alexia Rabé"
     },{
         "zone" : "yt-halo",
         "lien" : "https://www.youtube.com/channel/UCX47pe2Lprb0zFOFp1siDYw",
-        "msgbox" : "yt4"
+        "msgbox" : "yt4",
+        "text" : "d'Halo Music"
     },{
         "zone" : "yt-jeremyBesnard",
         "lien" : "https://www.youtube.com/channel/UC01Lbrb3W2p2YFns73D8JQw",
-        "msgbox" : "yt5"
+        "msgbox" : "yt5",
+        "text" : "de Jérèmy Besnard"
     },{
         "zone" : "yt-generationTemoin",
         "lien" : "https://www.youtube.com/channel/UCzMauwnWnPHpmiHSa5BobiQ",
-        "msgbox" : "yt6"
+        "msgbox" : "yt6",
+        "text" : "de Génération Témoin"
     },{
         "zone" : "yt-samuelOlivier",
         "lien" : "https://www.youtube.com/channel/UCD1abbs5BwD4fibdwGo4JRA",
-        "msgbox" : "yt7"
+        "msgbox" : "yt7",
+        "text" : "de Samuel Olivier"
     },{
         "zone" : "yt-elleLimbear",
         "lien" : "https://www.youtube.com/channel/UCW0lNu0ijva2yxnGGLT9QXg",
-        "msgbox" : "yt8"
+        "msgbox" : "yt8",
+        "text" : "d'Elle Limebear"
     },{
         "zone" : "yt-kabe",
         "lien" : "https://www.youtube.com/user/KaBeOGC",
-        "msgbox" : "yt9"
+        "msgbox" : "yt9",
+        "text" : "de Kabe"
     } 
 ];
 
@@ -45,7 +54,7 @@ arrayYT.forEach(artiste => {
     let ytPopUp;
     
     WA.onEnterZone(artiste.zone, () => {
-        ytPopUp = WA.openPopup(artiste.msgbox, "Voir la chaine Youtube de l'artiste.", [{
+        ytPopUp = WA.openPopup(artiste.msgbox, `Voir la chaine Youtube ${artiste.text}`, [{
             label: "Voir",
             className: "primary",
             callback: (popup) => {
@@ -58,6 +67,7 @@ arrayYT.forEach(artiste => {
         ytPopUp.close();
     });
 });
+
 
 WA.onEnterZone('zone-1', () => {
     donsPopUp = WA.openPopup("msg1", "Dieu nous a en effet mis à cœur de laisser l’entrée libre, et c'est grâce aux dons que chaque année le festival peut se dérouler.", [{
@@ -72,7 +82,6 @@ WA.onEnterZone('zone-1', () => {
 WA.onLeaveZone('zone-1', () => {
     donsPopUp.close();
 });
-
 
 
 WA.onEnterZone('zone-2', () => {
